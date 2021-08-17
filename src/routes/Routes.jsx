eacda5df0673d72da5e9b5b7edb8540d53b91bee
Maxime-Moilliet <mlltmax66@gmail.com>
2021-08-17 14:59:21 +0200

    feature: create Login form

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { Home } from '../screens/Home';
+import Login from '../screens/Login';
 
 export class Routes extends React.Component {
     render() {
         return (
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
             </Switch>
         );
     }
