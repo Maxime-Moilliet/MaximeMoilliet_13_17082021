f449863530723b561e6c45f94d9321c903a5dfc8
Maxime-Moilliet <mlltmax66@gmail.com>
2021-08-17 17:36:19 +0200

    refactoring: home cards

---
+++ src/data/homeCards.js
@@ -0,0 +1,23 @@
+export const homeCards = [
+    {
+      id: "1",
+      name: "You are our #1 priority",
+      desc: "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
+      img: "./images/icon-chat.png",
+      alt: "chat icon",
+    },
+    {
+      id: "2",
+      name: "More savings means higher rates",
+      desc: "The more you save with us, the higher your interest rate will be!",
+      img: "./images/icon-money.png",
+      alt: "money icon"
+    },
+    {
+      id: "3",
+      name: "Security you can trust",
+      desc: "We use top of the line encryption to make sure your data and money is always safe.",
+      img: "./images/icon-security.png",
+      alt: "security icon"
+    },
+  ];
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HomeCard } from '../components/HomeCard';
+import { homeCards } from '../data/homeCards';
 
 export class Home extends React.Component {
     render() {
@@ -16,24 +17,15 @@ export class Home extends React.Component {
                 </div>
                 <section className="features">
                     <h2 className="sr-only">Features</h2>
-                    <HomeCard 
-                        name="You are our #1 priority" 
-                        img="./images/icon-chat.png"
-                        alt="chat icon"
-                        desc="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
-                    />
-                    <HomeCard 
-                        name="More savings means higher rates" 
-                        img="./images/icon-money.png"
-                        alt="money icon"
-                        desc="The more you save with us, the higher your interest rate will be!"
-                    />
-                    <HomeCard 
-                        name="Security you can trust" 
-                        img="./images/icon-security.png"
-                        alt="security icon"
-                        desc="We use top of the line encryption to make sure your data and money is always safe."
-                    />
+                    {homeCards.map(card =>
+                        <HomeCard 
+                            key={card.id}
+                            name={card.name} 
+                            img={card.img}
+                            alt={card.alt}
+                            desc={card.desc}
+                        />    
+                    )}
                 </section>
             </main>
         );
